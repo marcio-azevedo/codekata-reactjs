387b79631ab688e48240b3737dbdc4513cfc493f
Márcio Azevedo <marcio.azevedo.83@gmail.com>
2015-12-22 17:07:09 +0000

    Implemented build (via Gulp), fixed dependencies.

--- src/Prologue.ReactJS.Web/Properties/AssemblyInfo.cs
+++ src/Prologue.ReactJS.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Prologue.ReactJS.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Márcio Azevedo 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
